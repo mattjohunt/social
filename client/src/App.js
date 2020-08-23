cad2a219e1d3dcc4d997d25791ec04d85abbe466
Matt25969 <matt.jo.hunt@gmail.com>
2020-08-23 11:14:06 +0000

    updated Structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import GridCard from './components/GridCard';
+import Example from './components/Example';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Example />
+      <GridCard />      
+    </div>
+  );
+}
+
+export default App;
